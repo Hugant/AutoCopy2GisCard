ffde2fec1a084268e264744a542aa641dad50bb2
Hugant <HugantMD@yandex.ru>
2017-09-22 21:55:36 +0700

    Style refactoring

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-if (localStorage.number == null || localStorage.number == undefined || localStorage.number == "") {	localStorage.number = 0;}chrome.extension.onMessage.addListener(	function(request, sender, sendResponse) {		switch (request.name) {			case "getCard":				console.log(JSON.parse(localStorage.card));				sendResponse(JSON.parse(localStorage.card));				break;							case "setCard":				request.value.number = localStorage.number;				localStorage.card = JSON.stringify(request.value);				break;							case "increaseNumber":				localStorage.number++;				break;							case "changeNumber":				localStorage.number = request.value;				break;							case "getNumber":				sendResponse(localStorage.number);				break;		}});
+if (localStorage.number == null ||
+    localStorage.number == undefined ||
+    localStorage.number == "") {
+    localStorage.number = 0;
+}
+
+chrome.extension.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        switch (request.name) {
+            case "getCard":
+                console.log(JSON.parse(localStorage.card));
+                sendResponse(JSON.parse(localStorage.card));
+                break;
+
+            case "setCard":
+                console.log(request.value);
+                request.value.number = localStorage.number;
+                localStorage.card = JSON.stringify(request.value);
+                break;
+
+            case "increaseNumber":
+                localStorage.number++;
+                break;
+
+            case "changeNumber":
+                localStorage.number = request.value;
+                break;
+
+            case "getNumber":
+                sendResponse(localStorage.number);
+                break;
+        }
+    });
